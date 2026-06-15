021b248a2f3c4b954fd5bc2e324c86930a91b4e2
xianghaoyang6-cyber <xianghaoyang6@gmail.com>
2026-06-15 18:11:33 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,49 @@
-const CACHE_NAME = 'jiayi-order-site-v1-9';
-const APP_SHELL = [
+const CACHE_NAME = 'qianggu-order-app-v1-11-icon-inline-fix';
+const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
+  './favicon.png',
   './icons/icon-192.png',
   './icons/icon-512.png',
-  './icons/apple-touch-icon.png',
-  './icons/maskable-512.png',
-  './default-menu-backup.json'
+  './icons/apple-touch-icon.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
-  );
+self.addEventListener('install', function (event) {
   self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(ASSETS);
+    })
   );
-  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (key) {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
 
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then(cached => {
+    caches.match(event.request).then(function (cached) {
       if (cached) return cached;
-      return fetch(event.request).then(response => {
+      return fetch(event.request).then(function (response) {
         const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, copy).catch(function () {});
+        });
         return response;
-      }).catch(() => caches.match('./index.html'));
+      }).catch(function () {
+        return caches.match('./index.html');
+      });
     })
   );
 });
